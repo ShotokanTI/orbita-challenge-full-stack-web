5a62053e1805993aaf82414afece1c58c433e137
Matheus <matheus.mnascimento@tivit.com>
2024-11-12 22:54:33 -0300

    Start the project by creating a student table and docker compose with postgres.

---
+++ backend/Desafio_A+_Educacao/Desafio_A+_Educacao/Entities/Student.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Desafio_A__Educacao.Entities
+{
+    public class Student
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(20)]
+        [Key]
+        [Column("RA", TypeName = "varchar(20)")]
+        public string RA { get; set; }  = string.Empty;
+
+        [Required]
+        [MaxLength(11)]
+        [Column("CPF", TypeName = "varchar(11)")]
+        public string CPF { get; set; } = string.Empty;
+    }
+}
---
+++ backend/Desafio_A+_Educacao/Desafio_A+_Educacao/Extensions/MigrationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Desafio_A__Educacao.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Desafio_A__Educacao.Extensions
+{
+    public static class MigrationExtensions
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+            using AppDbContext dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            dbContext.Database.Migrate();
+        }
+    }
+}
